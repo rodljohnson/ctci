3e1a7bbce6256749d6d431de31cf8ef37a4ee844
Rodney Johnson <rodney@Jacobs-Air.fios-router.home>
2019-04-25 00:24:56 -0400

    Remove question B

--- src/main/java/ch01/q2_check_permutation/QuestionB.java
+++
@@ -1,9 +0,0 @@
-package ch01.q2_check_permutation;
-
-public class QuestionB {
-
-    public static boolean permutation(String s, String t) {
-        throw new UnsupportedOperationException();
-    }
-
-}
