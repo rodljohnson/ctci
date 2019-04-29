217c31d9bb1e553c9ac6b194bb487d370ab3d688
Rodney Johnson <rodljohnson@hotmail.com>
2019-04-29 16:40:43 -0400

    Fix unit test ch01 q4

--- src/test/java/ch01/q4_palindrome_partition/PalindromePartitionTest.java
+++ src/test/java/ch01/q4_palindrome_partition/PalindromePartitionTest.java
@@ -10,12 +10,12 @@ public class PalindromePartitionTest {
     public static void isPermutationOfPalindrome(String str) {
         assertTrue(PalindromePartition.isPermutationOfPalindrome("Tact Coa"));
         assertTrue(PalindromePartition.isPermutationOfPalindrome("jhsabckuj ahjsbckj"));
-        assertTrue(PalindromePartition.isPermutationOfPalindrome("Able was I ere I saw Elb"));
+        assertFalse(PalindromePartition.isPermutationOfPalindrome("Able was I ere I saw Elb"));
         assertTrue(PalindromePartition.isPermutationOfPalindrome("So patient a nurse to nurse a patient s"));
         assertFalse(PalindromePartition.isPermutationOfPalindrome("Random Words"));
         assertFalse(PalindromePartition.isPermutationOfPalindrome("Not a Palindrome"));
-        assertFalse(PalindromePartition.isPermutationOfPalindrome("no x in nixon"));
-        assertFalse(PalindromePartition.isPermutationOfPalindrome("azAZ"));
+        assertTrue(PalindromePartition.isPermutationOfPalindrome("no x in nixon"));
+        assertTrue(PalindromePartition.isPermutationOfPalindrome("azAZ"));
     }
     
 }
