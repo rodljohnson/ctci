7196bfab9b4ff28b575527e6ad89512633f295bc
Rodney Johnson <rodljohnson@hotmail.com>
2019-05-08 12:24:28 -0400

    Fix ch1 q4 test case

--- src/test/java/ch01/Q04_PalindromePartitionTest.java
+++ src/test/java/ch01/Q04_PalindromePartitionTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class Q04_PalindromePartitionTest {
 
     @Test
-    public void isPermutationOfPalindrome(String str) {
+    public void isPermutationOfPalindrome() {
         assertTrue(Q04_PalindromePartition.isPermutationOfPalindrome("Tact Coa"));
         assertTrue(Q04_PalindromePartition.isPermutationOfPalindrome("jhsabckuj ahjsbckj"));
         assertFalse(Q04_PalindromePartition.isPermutationOfPalindrome("Able was I ere I saw Elb"));
