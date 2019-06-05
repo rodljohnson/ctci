1f8a6cdd8bb915ee5413253f5994d63bc5daf284
Rodney Johnson <rodney@Jacobs-Air.fios-router.home>
2019-06-05 08:40:23 -0400

    remove dups from linked list

---
+++ src/main/java/ch02/Q01_RemoveDups.java
@@ -0,0 +1,10 @@
+package ch02;
+
+import util.LinkedListNode;
+
+public class Q01_RemoveDups {
+
+    public static void deleteDups(LinkedListNode head) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/test/java/ch02/Q01_RemoveDupsTest.java
@@ -0,0 +1,23 @@
+package ch02;
+
+import org.junit.Test;
+import util.LinkedListNode;
+
+import static org.junit.Assert.*;
+
+public class Q01_RemoveDupsTest {
+
+    @Test
+    public void deleteDupsTest() {
+        LinkedListNode first = new LinkedListNode(0, null, null); //AssortedMethods.randomLinkedList(1000, 0, 2);
+        LinkedListNode head = first;
+        LinkedListNode second = first;
+        for (int i = 1; i < 8; i++) {
+            second = new LinkedListNode(i % 2, null, null);
+            first.setNext(second);
+            second.setPrevious(first);
+            first = second;
+        }
+        System.out.println(head.printForward());
+    }
+}
