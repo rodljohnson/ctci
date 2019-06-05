128f72627eeb4fca28880b17af0f6a90277883e3
Rodney Johnson <rodney@Jacobs-Air.fios-router.home>
2019-06-04 23:42:55 -0400

    set matrix to zero test cases

--- src/main/java/ch01/Q08_ZeroMatrix.java
+++ src/main/java/ch01/Q08_ZeroMatrix.java
@@ -2,7 +2,15 @@ package ch01;
 
 public class Q08_ZeroMatrix {
 
-    public static void setZeros(int[][] matrix) {
+    public static void setZerosA(int[][] matrix) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void setZerosB(int[][] matrix) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void setZerosC(int[][] matrix) {
         throw new UnsupportedOperationException();
     }
 
---
+++ src/test/java/ch01/Q08_ZeroMatrixTest.java
@@ -0,0 +1,62 @@
+package ch01;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import util.AssortedMethods;
+
+public class Q08_ZeroMatrixTest {
+
+    public static int[][] matrix;
+
+    @BeforeClass
+    public static void init() {
+        int nrows = 4;
+        int ncols = 4;
+        matrix = AssortedMethods.randomMatrix(nrows, ncols, -10, 10);
+        AssortedMethods.printMatrix(matrix);
+    }
+
+    @Test
+    public void  setZerosATest() {
+        int[][] clone = cloneMatrix(matrix);
+        Q08_ZeroMatrix.setZerosA(clone);
+
+        System.out.println();
+
+        AssortedMethods.printMatrix(clone);
+
+        System.out.println();
+    }
+
+    @Test
+    public void  setZerosBTest() {
+        int[][] clone = cloneMatrix(matrix);
+        Q08_ZeroMatrix.setZerosB(clone);
+
+        System.out.println();
+
+        AssortedMethods.printMatrix(clone);
+
+        System.out.println();
+    }
+
+    @Test
+    public void  setZerosCTest() {
+        int[][] clone = cloneMatrix(matrix);
+        Q08_ZeroMatrix.setZerosC(clone);
+
+        System.out.println();
+
+        AssortedMethods.printMatrix(clone);
+    }
+
+    public static int[][] cloneMatrix(int[][] matrix) {
+        int[][] c = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                c[i][j] = matrix[i][j];
+            }
+        }
+        return c;
+    }
+}
