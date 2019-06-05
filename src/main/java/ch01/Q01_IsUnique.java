21e528d36b308dd017748551efc6f2655919fabd
Rodney Johnson <rodney@Jacobs-MacBook-Air.local>
2019-06-05 11:08:56 -0400

    add test cases

--- src/main/java/ch01/Q01_IsUnique.java
+++ src/main/java/ch01/Q01_IsUnique.java
@@ -2,7 +2,11 @@ package ch01;
 
 public class Q01_IsUnique {
 
-    public static boolean isUniqueChars(String str) {
+    public static boolean isUniqueCharsA(String str) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean isUniqueCharsB(String str) {
         throw new UnsupportedOperationException();
     }
 
--- src/main/java/ch01/Q02_CheckPermutation.java
+++ src/main/java/ch01/Q02_CheckPermutation.java
@@ -2,7 +2,11 @@ package ch01;
 
 public class Q02_CheckPermutation {
 
-    public static boolean permutation(String s, String t) {
+    public static boolean permutationA(String s, String t) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean permutationB(String s, String t) {
         throw new UnsupportedOperationException();
     }
 
