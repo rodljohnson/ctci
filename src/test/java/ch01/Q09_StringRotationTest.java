7afd8c30b83891a064cbd00b5769e13fdf56e16b
Rodney Johnson <rodljohnson@hotmail.com>
2019-06-05 14:53:45 -0400

    add unit test

--- src/test/java/ch01/Q09_StringRotationTest.java
+++ src/test/java/ch01/Q09_StringRotationTest.java
@@ -9,8 +9,18 @@ public class Q09_StringRotationTest {
     @Test
     public void isRotationTest() {
         String[][] pairs = {{"apple", "pleap"}, {"waterbottle", "erbottlewat"}, {"camera", "macera"}};
-        assertTrue(Q09_StringRotation.isRotation(pairs[0][0], pairs[0][1]));
-        assertTrue(Q09_StringRotation.isRotation(pairs[1][0], pairs[1][1]));
-        assertFalse(Q09_StringRotation.isRotation(pairs[2][0], pairs[2][1]));
+        assertTrue(Q09_StringRotation.isRotationA(pairs[0][0], pairs[0][1]));
+        assertTrue(Q09_StringRotation.isRotationA(pairs[1][0], pairs[1][1]));
+        assertFalse(Q09_StringRotation.isRotationA(pairs[2][0], pairs[2][1]));
+        assertTrue(Q09_StringRotation.isRotationB(pairs[0][0], pairs[0][1]));
+        assertTrue(Q09_StringRotation.isRotationB(pairs[1][0], pairs[1][1]));
+        assertFalse(Q09_StringRotation.isRotationB(pairs[2][0], pairs[2][1]));
     }
 }
+
+//apple
+//pplea
+//pleap
+//leapp
+//eappl
+//apple
