7da2ebd75555993380c492fdac41369cbacfe188
Rodney Johnson <rodljohnson@hotmail.com>
2019-06-06 19:32:20 -0400

    add unit test

--- src/main/java/ch02/Q05_SumLists.java
+++ src/main/java/ch02/Q05_SumLists.java
@@ -1,4 +1,10 @@
 package ch02;
 
+import util.LinkedListNode;
+
 public class Q05_SumLists {
+
+    public static LinkedListNode addLists(LinkedListNode l1, LinkedListNode l2) {
+        throw new UnsupportedOperationException();
+    }
 }
--- src/test/java/ch02/Q05_SumListsTest.java
+++ src/test/java/ch02/Q05_SumListsTest.java
@@ -1,7 +1,43 @@
 package ch02;
 
+import org.junit.Test;
+import util.LinkedListNode;
+
 import static org.junit.Assert.*;
 
 public class Q05_SumListsTest {
+
+    @Test
+    public void addListsTest() {
+        LinkedListNode lA1 = new LinkedListNode(9, null, null);
+        LinkedListNode lA2 = new LinkedListNode(9, null, lA1);
+        LinkedListNode lA3 = new LinkedListNode(9, null, lA2);
+
+        LinkedListNode lB1 = new LinkedListNode(1, null, null);
+        LinkedListNode lB2 = new LinkedListNode(0, null, lB1);
+        LinkedListNode lB3 = new LinkedListNode(0, null, lB2);
+
+        LinkedListNode list3 = Q05_SumLists.addLists(lA1, lB1);
+
+        System.out.println("  " + lA1.printForward());
+        System.out.println("+ " + lB1.printForward());
+        System.out.println("= " + list3.printForward());
+
+        int l1 = linkedListToInt(lA1);
+        int l2 = linkedListToInt(lB1);
+        int l3 = linkedListToInt(list3);
+
+        System.out.print(l1 + " + " + l2 + " = " + l3 + "\n");
+        System.out.print(l1 + " + " + l2 + " = " + (l1 + l2));
+    }
+
+    private static int linkedListToInt(LinkedListNode node) {
+        int res = 0;
+        LinkedListNode cur = node;
+        while(cur != null) {
+            res = (res * 10) + cur.data;
+        }
+        return res;
+    }
 
 }
