9d3072bfbf01f4d61fab0aec46a9feaf95f2ebc8
Rodney Johnson <rodljohnson@hotmail.com>
2019-06-08 12:57:19 -0400

    add unit test

--- src/main/java/ch02/Q06_Palindrome.java
+++ src/main/java/ch02/Q06_Palindrome.java
@@ -4,14 +4,17 @@ import util.LinkedListNode;
 
 public class Q06_Palindrome {
 
+    //iterative stack
     public static boolean isPalindromeA(LinkedListNode head) {
         throw new UnsupportedOperationException();
     }
 
+    //reverse and clone
     public static boolean isPalindromeB(LinkedListNode head) {
         throw new UnsupportedOperationException();
     }
 
+    //recursive
     public static boolean isPalindromeC(LinkedListNode head) {
         throw new UnsupportedOperationException();
     }
