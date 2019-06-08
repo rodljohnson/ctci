29885e66d7b96356cbfd03bdac5ff2a029fae983
Rodney Johnson <rodljohnson@hotmail.com>
2019-06-08 14:35:12 -0400

    add unit test

--- src/main/java/ch02/Q08_LoopDetection.java
+++ src/main/java/ch02/Q08_LoopDetection.java
@@ -1,4 +1,14 @@
 package ch02;
 
+import util.LinkedListNode;
+
 public class Q08_LoopDetection {
+
+    public static LinkedListNode findBeginningA(LinkedListNode head) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static LinkedListNode findBeginningB(LinkedListNode head) {
+        throw new UnsupportedOperationException();
+    }
 }
--- src/test/java/ch02/Q08_LoopDetectionTest.java
+++ src/test/java/ch02/Q08_LoopDetectionTest.java
@@ -1,7 +1,37 @@
 package ch02;
 
+import org.junit.Test;
+import util.LinkedListNode;
+
 import static org.junit.Assert.*;
 
 public class Q08_LoopDetectionTest {
 
+    @Test
+    public void findBeginningTest() {
+        int list_length = 10;
+        int k = 9;
+
+        // Create linked list
+        LinkedListNode[] nodes = new LinkedListNode[list_length];
+        for (int i = 0; i < list_length; i++) {
+            nodes[i] = new LinkedListNode(i, null, i > 0 ? nodes[i - 1] : null);
+        }
+
+        // Create loop;
+        nodes[list_length - 1].next = nodes[list_length - k];
+
+        LinkedListNode loopA = Q08_LoopDetection.findBeginningA(nodes[0]);
+        if (loopA == null) {
+            System.out.println("No Cycle.");
+        } else {
+            System.out.println(loopA.data);
+        }
+        LinkedListNode loopB = Q08_LoopDetection.findBeginningB(nodes[0]);
+        if (loopB == null) {
+            System.out.println("No Cycle.");
+        } else {
+            System.out.println(loopB.data);
+        }
+    }
 }
