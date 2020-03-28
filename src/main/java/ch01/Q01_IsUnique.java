5d4800468cfc8c72b6f2815e5fb25010b5fd7181
Rodney Johnson <rodljohnson@hotmail.com>
2020-03-28 19:00:52 -0400

    Chapter 1 question 1 solution

--- src/main/java/ch01/Q01_IsUnique.java
+++ src/main/java/ch01/Q01_IsUnique.java
@@ -1,12 +1,35 @@
 package ch01;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Implement an algorithm to determine if a string has all unique characters.
  */
 class Q01_IsUnique {
 
+  //O(n) time
+  //O(n) space
   public boolean isUniqueChars(String str) {
-    throw new UnsupportedOperationException();
+    Set<Character> set = new HashSet<>();
+    for(char c : str.toCharArray()) {
+      if(!set.add(c))
+        return false;
+    }
+    return true;
   }
+
+  //O(n^2) time
+  //O(1) space
+  /*public boolean isUniqueChars(String str) {
+    for(int i = 0;i < str.length();i++) {
+      for(int j = i + 1;j < str.length();j++) {
+       if(str.charAt(i) == str.charAt(j)) {
+         return false;
+       }
+      }
+    }
+    return true;
+  }*/
 
 }
