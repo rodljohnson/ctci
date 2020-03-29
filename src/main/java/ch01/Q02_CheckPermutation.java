39f8e125110fa45b127902793e5cc5aa14717653
Rodney Johnson <rodljohnson@hotmail.com>
2020-03-29 08:19:46 -0400

    Solution Chapter 1 question 1

--- src/main/java/ch01/Q02_CheckPermutation.java
+++ src/main/java/ch01/Q02_CheckPermutation.java
@@ -1,12 +1,39 @@
 package ch01;
 
+import java.util.Arrays;
+
 /**
  * Check Permutation: Given two strings,write a method to decide if one is a permutation of the other.
  */
 class Q02_CheckPermutation {
 
+  //O(nlog(n)) time
+  // O(1) space
   public boolean permutation(String s, String t) {
-    throw new UnsupportedOperationException();
+    return sort(s).equals(sort(t));
   }
+
+  private String sort(String str) {
+    char[] arr = str.toCharArray();
+    Arrays.sort(arr);
+    return new String(arr);
+  }
+
+  //O(n) time
+  // O(n) space
+  /*public boolean permutation(String s, String t) {
+    if(s.length() != t.length())
+      return false;
+    int[] buf = new int[128];
+    for(char c : s.toCharArray()) {
+      buf[c - 'a']++;
+    }
+    for(char c : t.toCharArray()) {
+      buf[c - 'a']--;
+      if(buf[c - 'a'] < 0)
+        return false;
+    }
+    return true;
+  }*/
 
 }
