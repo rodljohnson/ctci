a84fd9ae035d6cbde21c87d0a5e8b312543c5d1e
Rodney Johnson <rodljohnson@hotmail.com>
2020-03-29 07:52:47 -0400

    Update Chapter 1 questions

--- src/main/java/ch01/Q09_StringRotation.java
+++ src/main/java/ch01/Q09_StringRotation.java
@@ -1,7 +1,7 @@
 package ch01;
 
 /**
- * Assume you have a method isSubstring which checks if one word is a substring of another. Given
+ * String Rotation; Assume you have a method isSubstring which checks if one word is a substring of another. Given
  * two strings, sl and s2, write code to check if s2 is a rotation of s1 using only one call to
  * isSubstring (e.g.,"waterbottle" is a rotation of"erbottlewat").
  */
