64677d6adce4f950df557b80c078a12c4b40a0bb
Rodney Johnson <rodljohnson@hotmail.com>
2020-03-30 21:23:53 -0400

    Add solution chapter 1 question 5

--- src/main/java/ch01/Q05_OneAway.java
+++ src/main/java/ch01/Q05_OneAway.java
@@ -10,6 +10,30 @@ package ch01;
 class Q05_OneAway {
 
   public boolean oneEditAway(String a, String b) {
-    throw new UnsupportedOperationException();
+    if(Math.abs(a.length() - b.length()) > 1) {
+      return false;
+    }
+    String s1 = (a.length() < b.length()) ? b : a;
+    String s2 = (a.length() < b.length()) ? a : b;
+    int index1 = 0;
+    int index2 = 0;
+    boolean isOne = false;
+    while(index2 < s2.length()) {
+      if(s1.charAt(index1) != s2.charAt(index2)) {
+        if(isOne)
+          return false;
+        isOne = true;
+        if(s1.length() > s2.length()) {
+          index1++;
+        } else {
+          index1++;
+          index2++;
+        }
+      } else {
+        index1++;
+        index2++;
+      }
+    }
+    return true;
   }
 }
