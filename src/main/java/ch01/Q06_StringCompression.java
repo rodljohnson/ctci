eb0dbb8fde4b676d35538fbcfcc130ab6c148e99
Rodney Johnson <rodljohnson@hotmail.com>
2020-04-04 23:20:55 -0400

    Add solution chapter 1 question 6

--- src/main/java/ch01/Q06_StringCompression.java
+++ src/main/java/ch01/Q06_StringCompression.java
@@ -9,7 +9,17 @@ package ch01;
 class Q06_StringCompression {
 
   public String compress(String str) {
-    throw new UnsupportedOperationException();
+    StringBuilder sb = new StringBuilder();
+    int count = 0;
+    for(int i = 0;i < str.length();i++) {
+      count++;
+      if(i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+        sb.append(str.charAt(i));
+        sb.append(count);
+        count = 0;
+      }
+    }
+    return (sb.length() < str.length()) ? sb.toString() : str;
   }
 
 }
