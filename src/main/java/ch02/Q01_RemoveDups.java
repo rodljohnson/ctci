326b604cd54f93182cb82e0a6b54af54f2beeb46
Rodney Johnson <rodljohnson@hotmail.com>
2020-04-16 14:23:06 -0400

    Add solution to ch02 question 1

--- src/main/java/ch02/Q01_RemoveDups.java
+++ src/main/java/ch02/Q01_RemoveDups.java
@@ -1,12 +1,49 @@
 package ch02;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Remove Dups! Write code to remove duplicates from an unsorted linked list.
  */
 class Q01_RemoveDups {
 
+  //O(n^2) runtime
+  //O(1) space
   public LinkedListNode deleteDups(LinkedListNode head) {
-    throw new UnsupportedOperationException();
+    if(head == null || head.next == null) {
+      return head;
+    }
+    LinkedListNode cur = head;
+    while(cur != null) {
+      LinkedListNode temp = cur;
+      while(temp != null && temp.next != null) {
+        if(temp.next.val == cur.val) {
+          temp.next = temp.next.next;
+        }
+        temp = temp.next;
+      }
+      cur = cur.next;
+    }
+    return head;
   }
+
+  //O(n) runtime
+  //O(n) space
+  /*public LinkedListNode deleteDups(LinkedListNode head) {
+    if(head == null || head.next == null) {
+      return head;
+    }
+    Set<Integer> set = new HashSet<>();
+    LinkedListNode cur = head;
+    while(cur != null && cur.next != null) {
+      set.add(cur.val);
+      if(set.contains(cur.next.val)) {
+        cur.next = cur.next.next;
+      }
+      cur = cur.next;
+    }
+    return head;
+  }*/
 
 }
