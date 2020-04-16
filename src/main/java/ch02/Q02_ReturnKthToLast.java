317826ff933c000135f87fec78502142237a8691
Rodney Johnson <rodljohnson@hotmail.com>
2020-04-16 16:47:56 -0400

    Add question 2 chapter 2 solution

--- src/main/java/ch02/Q02_ReturnKthToLast.java
+++ src/main/java/ch02/Q02_ReturnKthToLast.java
@@ -5,8 +5,37 @@ package ch02;
  */
 class Q02_ReturnKthToLast {
 
+  //iterative
   public LinkedListNode kthToLast(LinkedListNode head, int k) {
-    throw new UnsupportedOperationException();
+    LinkedListNode l1 = head;
+    for(int i = 0;i <= k;i++) {
+      if(l1 == null)
+        return null;
+      l1 = l1.next;
+    }
+    LinkedListNode l2 = head;
+    while(l1 != null) {
+      l1 = l1.next;
+      l2 = l2.next;
+    }
+    return l2;
   }
+
+  //recursive
+  /*public LinkedListNode kthToLast(LinkedListNode head, int k) {
+    return helper(head, k, new int[]{-1});
+  }
+
+  private LinkedListNode helper(LinkedListNode head, int k, int[] index) {
+    if(head == null) {
+      return null;
+    }
+    LinkedListNode res = helper(head.next, k, index);
+    index[0] = index[0] + 1;
+    if(index[0] == k) {
+      return head;
+    }
+    return res;
+  }*/
 
 }
